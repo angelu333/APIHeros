b983cf1c7844db2ed52196c1b59651489f7f37c9
angelu333 <angelu333@gmail.com>
2025-07-22 19:30:26 -0600

    fix: actualizo contraseña de MongoDB Atlas en db.js

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/api-superheroes';
-
-export async function connectDB() {
+export const connectDB = async () => {
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect('mongodb+srv://Angel0407:Perra2002xd@cluster0.g6gbqv9.mongodb.net/api-superheroes?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Conectado a MongoDB Atlas');
   } catch (error) {
     console.error('Error conectando a MongoDB:', error);
-    process.exit(1);
   }
-} 
+}; 
